a124d00c4b6e137c3d17d162861c03772bf1e1e6
mompei <ogunborjerry@gmail.com>
2025-04-29 05:05:01 +0100

    Updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use instructions::EscrowInstructions;
 use pinocchio::{
     ProgramResult, account_info::AccountInfo, entrypoint, program_error::ProgramError,
     pubkey::Pubkey,
@@ -5,6 +6,8 @@ use pinocchio::{
 
 pub mod instructions;
 pub mod state;
+
+entrypoint!(process_instruction);
 
 use pinocchio_pubkey::declare_id;
 declare_id!("FFEfkkRGnefuA2TxPSGDbjV9cgBrWM3EtspEuxtYFkA");
@@ -14,5 +17,19 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    assert_eq!(program_id, &id());
+
+    let (discriminator, data) = instruction_data
+        .split_first()
+        .ok_or(ProgramError::InvalidAccountData)?;
+
+    match EscrowInstructions::try_from(*discriminator)? {
+        EscrowInstructions::Make => instructions::process_make_instruction(accounts, data)?,
+        EscrowInstructions::DepositY => {
+            instructions::process_deposit_y_instruction(accounts, data)?
+        }
+        EscrowInstructions::Take => instructions::process_take_instruction(accounts, data)?,
+        EscrowInstructions::Refund => instructions::process_refund_instruction(accounts, data)?,
+    }
     Ok(())
 }
