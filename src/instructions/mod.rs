5f99d6cd4ecf2024bcb35c1e8e244052ff8ea1a3
mompei <ogunborjerry@gmail.com>
2025-04-29 04:56:57 +0100

    Adjustments to mod.rs

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -6,3 +6,26 @@ pub use deposit_y::*;
 pub use make::*;
 pub use refund::*;
 pub use take::*;
+
+use pinocchio::program_error::ProgramError;
+
+pub enum EscrowInstructions {
+    Make = 0,
+    DepositY = 1,
+    Take = 2,
+    Refund = 3,
+}
+
+impl TryFrom<u8> for EscrowInstructions {
+    type Error = ProgramError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(EscrowInstructions::Make),
+            1 => Ok(EscrowInstructions::DepositY),
+            2 => Ok(EscrowInstructions::Take),
+            3 => Ok(EscrowInstructions::Refund),
+            _ => Err(ProgramError::InvalidInstructionData),
+        }
+    }
+}
